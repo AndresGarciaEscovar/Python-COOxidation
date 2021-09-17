2319da04ef4242fce4ef5b819c1f43b44a7d9354
Andres Garcia <andrumen1@yahoo.com>
2021-09-17 17:48:20 -0500

    Work in the order reduction algorithm.

--- tests.py
+++ tests.py
@@ -1,10 +1,30 @@
 import itertools
 
+state_list = [(('E', 2), ('E', 3)),
+              (('E', 2), ('E', 3)),
+              (('CO', 1), ('E', 2), ('E', 3)),
+              (('CO', 1), ('E', 2), ('E', 3)),
+              (('O', 1), ('E', 2), ('E', 3)),
+              (('O', 1), ('E', 2), ('E', 3)),
+              (('E', 1), ('E', 2), ('E', 3)),
+              (('E', 1), ('E', 2), ('E', 3)),
+              (('E', 1), ('E', 2), ('E', 3))]
 
-state = ((('CO', 1),('CO', 2), ('CO', 3)),
-         (('CO', 1),('CO', 2), ('O', 3)),
-         (('CO', 1),('CO', 2), ('E', 3)),
-         (('CO', 1),('O', 2), ('E', 3)),
-         )
 
-print((("CO", 1),("CO", 2),("CO", 3)) in state)
+# Auxiliary variables.
+candidates = []
+
+# Gather the tuples in groups of three.
+combs = list(itertools.combinations(state_list, 3))
+for comb in combs:
+    # Only look a the tuples that have elements of the same length.
+    if len(set(tuple(map(len, comb)))) == 1:
+        candidates.append(comb)
+
+# No more need for this variable.
+del combs
+
+for candidate in candidates:
+    for elem in candidate:
+        print(elem)
+    print("")
