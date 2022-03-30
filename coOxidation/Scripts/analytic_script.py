c308ff194feaee1b6330d172957d363169f5d35f
Andres <andrumen1@yahoo.com>
2022-03-30 09:59:12 -0500

    Nothing of importance.

--- coOxidation/Scripts/analytic_script.py
+++ coOxidation/Scripts/analytic_script.py
@@ -6,16 +6,16 @@ from coOxidation.Program.Analytic.equation_generator import EquationGenerator
 
 if __name__ == "__main__":
     # Define the states and the number of sites.
-    sites_number = 2
+    sites_number = 3
 
-    # Define the equation order.
+    # Define the equation order (i.e., 0 = Exact, 1 = MF, 2 = PA, ...).
     order = 2
 
     # Create a CO oxidation system with three sites.
     system = EquationGenerator(sites_number)
 
-    # Get the nth order equations.
-    system.order = 2
+    # Get the nth order equations .
+    system.order = order
     system.get_nth_order_equations(display=True)
 
     # Generate the equations for the given format.
